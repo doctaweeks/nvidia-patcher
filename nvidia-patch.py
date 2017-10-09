e62a9d18bc83ae4665f9b8f065a01fa9fd1274d9
Daniel M. Weeks <dan@danweeks.net>
2017-10-09 12:04:02 -0400

    Indicate when module has already been patched

--- nvidia-patch.py
+++ nvidia-patch.py
@@ -10,8 +10,13 @@ with open(orig, mode='a+') as f:
     with mmap.mmap(f.fileno(), 0) as s:
         idx = s.find(b'KVMKVMKVM')
         if idx == -1:
-            print('Error: did not find signature', file=sys.stderr)
-            sys.exit(1)
+            idx = s.find(b'AAAAAAAAA')
+            if idx == -1:
+                print('Error: did not find signature', file=sys.stderr)
+                sys.exit(1)
+            else:
+                print('Already patched', file=sys.stderr)
+                sys.exit(0)
         s.seek(idx)
         s.write(b'AAAAAAAAA')
         s.flush()
